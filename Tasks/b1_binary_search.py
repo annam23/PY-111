04e1522f174fdd1f0fbde3a32aa0e3a2287817f1
annam23 <mashunina@outlook.com>
2022-02-19 11:21:27 +0300

    b1 done

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,25 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    #Определяем границы поиска
+    start_index = 0
+    end_index = len(arr) - 1
+
+    #Условие, пока возможен поиск
+    while start_index <= end_index:
+
+    #Определяем, как делить последовательность
+        middle_index = (start_index + end_index) // 2
+        middle_elem = arr[middle_index]
+
+    #Рассматриваем сценарии поиска:
+        if middle_elem == elem:
+            return middle_elem
+        elif middle_elem > elem:
+            end_index = middle_index
+        elif middle_elem < elem:
+            start_index = middle_index
+
+
+
+

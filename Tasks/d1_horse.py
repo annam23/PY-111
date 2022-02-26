fa61bda996a69160e6bc336a85c156cb05c1386a
annam23 <mashunina@outlook.com>
2022-02-26 12:51:55 +0300

    d1 create a chess board

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,13 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 0
+    board = [[0] * shape[0] for i in range(shape[1])]
+    board[0][0] = 1
+
+    for i in range(0, len(board)):
+        for j in range(0, len(board[i])):
+            print(board[i][j], end=' ')
+        print()
+
+
+calculate_paths((3,6), (1,1))

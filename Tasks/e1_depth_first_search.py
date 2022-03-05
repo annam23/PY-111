660f3c6a78a39e6fc52c4d851ac47383d1b41b5b
annam23 <mashunina@outlook.com>
2022-03-05 21:52:24 +0300

    e1 done

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -10,5 +10,20 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    stack_nodes = list()
+    stack_nodes.append(start_node)
+
+    result_nodes = []
+    visited_nodes = {start_node}
+
+    while stack_nodes:
+        current_node = stack_nodes.pop()
+
+        for neighbour in g[current_node]:
+            if neighbour not in visited_nodes:
+                stack_nodes.append(neighbour)
+                visited_nodes.add(neighbour)
+
+        result_nodes.append(current_node)
+
+    return result_nodes

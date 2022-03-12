465cb6d271bde8de8a47a529791eb1f5bac5315e
Alexeysemeno <cemen90@gmail.com>
2022-03-12 19:01:19 +0300

    сделано и разобрано на практике

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -8,4 +8,8 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
+    for j in range(len(container)):
+        for i in range(len(container) - 1 - j):
+            if container[i] > container[i+1]:
+                container[i], container[i+1] = container[i+1], container[i]
     return container

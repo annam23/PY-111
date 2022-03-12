ba43acca0080dc892bcdc1921d8b8524fdcde90b
annam23 <mashunina@outlook.com>
2022-03-12 11:40:18 +0300

    поменяны местами столбцы/строки

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,7 +6,7 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    board = [[0] * shape[0] for i in range(shape[1])]
+    board = [[0] * shape[1] for i in range(shape[0])]
     board[0][0] = 1
     CONST_WAY = 2
 

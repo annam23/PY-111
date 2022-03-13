8362710ef6f8da00f979b4655afeb4d281fc4ddc
Alexeysemeno <cemen90@gmail.com>
2022-03-13 16:34:05 +0300

    g1 done

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 def sort(container: List[int]) -> List[int]:
     """
     Sort input container with merge sort
@@ -8,4 +7,26 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+    result_container = []
+
+    if len(container) < 2:
+        return container
+
+    middle_ind = len(container) // 2
+    right, left = sort(container[middle_ind:]), sort(container[:middle_ind])
+
+    index_left = 0
+    index_right = 0
+
+    while index_left < len(left) and index_right < len(right):
+        if left[index_left] < right[index_right]:
+            result_container.append(left[index_left])
+            index_left += 1
+        else:
+            result_container.append(right[index_right])
+            index_right += 1
+
+    result_container.extend(right[index_right:])
+    result_container.extend(left[index_left:])
+
+    return result_container
